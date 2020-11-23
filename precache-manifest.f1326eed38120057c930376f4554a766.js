ab65cd300b9f1b5b8ab3eca15175c3c2eb08a9dc
Tiron <ole_g92@bk.ru>
2020-11-24 00:03:59 +0300

    Updates

---
+++ precache-manifest.f1326eed38120057c930376f4554a766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47abd50ff5de533233bbee8d52f7bfe",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "16647ef569f9d210f131",
+    "url": "/reactTest/static/css/main.d87a8c72.chunk.css"
+  },
+  {
+    "revision": "c7473a49d33252e1c206",
+    "url": "/reactTest/static/js/2.7c56ff23.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactTest/static/js/2.7c56ff23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16647ef569f9d210f131",
+    "url": "/reactTest/static/js/main.2912e0d5.chunk.js"
+  },
+  {
+    "revision": "42883c227b9e8d7a8d8a",
+    "url": "/reactTest/static/js/runtime-main.fbb78b55.js"
+  }
+]);
